cb2e37b53d5df4e15ca311b7b6c2d0373bb7e406
Dave Lin <dave.lin21@gmail.com>
2023-09-01 16:54:06 -0400

    todo sample

--- src/app/todo/page.jsx
+++ src/app/todo/page.jsx
@@ -1,5 +1,41 @@
+'use client';
+import { useState } from 'react';
+
 const Todo = () => {
-  return <div>Todo</div>;
+  const [search, setSearch] = useState('');
+  const [itemToAdd, setItemToAdd] = useState('');
+  const [list, setList] = useState([]);
+
+  const addItem = (e) => {
+    e.preventDefault();
+    if (itemToAdd) {
+      setList([...list, itemToAdd]);
+    }
+    setItemToAdd('');
+  };
+
+  const handleDelete = (item) => {
+    setList(list.filter((listItem) => listItem !== item));
+  };
+
+  return (
+    <div>
+      <input onChange={(e) => setSearch(e.target.value)} />
+      {list
+        .filter((listItem) => listItem.includes(search))
+        .map((itm, idx) => (
+          <li key={idx} onClick={() => handleDelete(itm)}>
+            {itm}
+          </li>
+        ))}
+      <form onSubmit={(e) => addItem(e)}>
+        <input
+          onChange={(e) => setItemToAdd(e.target.value)}
+          value={itemToAdd}
+        />
+      </form>
+    </div>
+  );
 };
 
 export default Todo;
