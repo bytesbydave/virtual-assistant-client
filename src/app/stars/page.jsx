da7be34aa8c5e64bd10d62c303912b4f80cff50a
Dave Lin <dave.lin21@gmail.com>
2023-09-26 23:20:39 -0400

    fix stars

--- src/app/stars/page.jsx
+++ src/app/stars/page.jsx
@@ -19,27 +19,18 @@ const RatingStars = () => {
     <div className='container'>
       <h1 className='result'>Stars</h1>
       {GRADES.map((grade, index) => {
-        if (index < rating) {
-          return (
-            <div
-            // onMouseLeave={() => resetRating()}
-            >
-              Yellow Star
-            </div>
-          );
-        } else {
-          return (
-            <button
-              onClick={() => assignRating(index)}
-              // onMouseEnter={() => assignRating(index)}
-            >
-              <Star key={index} />
-            </button>
-          );
-        }
+        return (
+          <button
+            key={index}
+            onClick={() => assignRating(index)}
+            // onMouseEnter={() => assignRating(index)}
+          >
+            <Star key={index} fill={index < rating ? 'yellow' : ''} />
+          </button>
+        );
       })}
       <button onClick={() => resetRating()}>Reset Rating</button>
-      <div>{GRADES[rating]}</div>
+      <div>{GRADES[rating - 1]}</div>
     </div>
   );
 };
