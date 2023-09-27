de93f67471a7fa26cb7f2d51d04c9183d0de0fd8
Dave Lin <dave.lin21@gmail.com>
2023-09-26 22:57:08 -0400

    comment out giphy

--- src/app/giphy/[id]/page.jsx
+++ src/app/giphy/[id]/page.jsx
@@ -1,107 +1,107 @@
-'use client';
+// 'use client';
 
-import Link from 'next/link';
-import Image from 'next/image';
-import { useEffect, useState } from 'react';
-import giphy from '../../apis/giphy';
-import Loading from '../../loading';
-import Error from '../../error';
+// import Link from 'next/link';
+// import Image from 'next/image';
+// import { useEffect, useState } from 'react';
+// import giphy from '../../apis/giphy';
+// import Loading from '../../loading';
+// import Error from '../../error';
 
-const generateGiphyDetails = (gif) => {
-  const giphyDetails = {
-    avatar: gif.user?.avatar_url || '',
-    displayName: gif.user?.display_name || 'N/A',
-    profileUrl: gif.user?.profile_url || '',
-    title: gif.title,
-    url: gif.url,
-    mp4: gif.images.original.mp4,
-    width: gif.images.original.width,
-    height: gif.images.original.height,
-    frames: gif.images.original.frames,
-  };
-  return giphyDetails;
-};
+// const generateGiphyDetails = (gif) => {
+//   const giphyDetails = {
+//     avatar: gif.user?.avatar_url || '',
+//     displayName: gif.user?.display_name || 'N/A',
+//     profileUrl: gif.user?.profile_url || '',
+//     title: gif.title,
+//     url: gif.url,
+//     mp4: gif.images.original.mp4,
+//     width: gif.images.original.width,
+//     height: gif.images.original.height,
+//     frames: gif.images.original.frames,
+//   };
+//   return giphyDetails;
+// };
 
-const GiphyDetail = ({ params }) => {
-  const [giphyDetails, setGiphyDetails] = useState([]);
-  const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState('');
-  const { id } = params;
+// const GiphyDetail = ({ params }) => {
+//   const [giphyDetails, setGiphyDetails] = useState([]);
+//   const [isLoading, setIsLoading] = useState(false);
+//   const [error, setError] = useState('');
+//   const { id } = params;
 
-  useEffect(() => {
-    async function fetchGiphyData() {
-      setIsLoading(true);
-      try {
-        const response = await giphy.get('/v1/gifs', {
-          params: {
-            ids: id,
-          },
-        });
-        setGiphyDetails(response.data.data);
-        setIsLoading(false);
-      } catch (err) {
-        setError(err.message);
-        setIsLoading(false);
-      }
-    }
-    fetchGiphyData();
-  }, [id]);
+//   useEffect(() => {
+//     async function fetchGiphyData() {
+//       setIsLoading(true);
+//       try {
+//         const response = await giphy.get('/v1/gifs', {
+//           params: {
+//             ids: id,
+//           },
+//         });
+//         setGiphyDetails(response.data.data);
+//         setIsLoading(false);
+//       } catch (err) {
+//         setError(err.message);
+//         setIsLoading(false);
+//       }
+//     }
+//     fetchGiphyData();
+//   }, [id]);
 
-  if (isLoading) {
-    return <Loading />;
-  }
+//   if (isLoading) {
+//     return <Loading />;
+//   }
 
-  if (error) {
-    return <Error errorMessage={error} />;
-  }
+//   if (error) {
+//     return <Error errorMessage={error} />;
+//   }
 
-  if (giphyDetails.length > 0) {
-    const details = generateGiphyDetails(giphyDetails[0]);
-    const {
-      avatar,
-      displayName,
-      profileUrl,
-      title,
-      url,
-      mp4,
-      width,
-      height,
-      frames,
-    } = details;
-    return (
-      <div>
-        <video width={width} height={height} autoPlay muted loop>
-          <source src={mp4} type='video/mp4' />
-        </video>
-        <div>
-          <Link href={url} target='_blank'>
-            <p>{`Title: ${title}`}</p>
-          </Link>
-        </div>
-        <div>
-          <p>{`Height: ${height}, Width: ${width}, Frames: ${frames}`}</p>
-        </div>
+//   if (giphyDetails.length > 0) {
+//     const details = generateGiphyDetails(giphyDetails[0]);
+//     const {
+//       avatar,
+//       displayName,
+//       profileUrl,
+//       title,
+//       url,
+//       mp4,
+//       width,
+//       height,
+//       frames,
+//     } = details;
+//     return (
+//       <div>
+//         <video width={width} height={height} autoPlay muted loop>
+//           <source src={mp4} type='video/mp4' />
+//         </video>
+//         <div>
+//           <Link href={url} target='_blank'>
+//             <p>{`Title: ${title}`}</p>
+//           </Link>
+//         </div>
+//         <div>
+//           <p>{`Height: ${height}, Width: ${width}, Frames: ${frames}`}</p>
+//         </div>
 
-        <div>
-          {avatar ? (
-            <Image alt={displayName} src={avatar} width='100' height='100' />
-          ) : (
-            <p>{`No Avatar Available`}</p>
-          )}
-        </div>
-        <div>
-          {profileUrl ? (
-            <Link href={profileUrl} target='_blank'>
-              <p>{`User: ${displayName}`}</p>
-            </Link>
-          ) : (
-            <p>{`User: ${displayName}`}</p>
-          )}
-        </div>
-      </div>
-    );
-  }
-  return <div>No gif available</div>;
-};
+//         <div>
+//           {avatar ? (
+//             <Image alt={displayName} src={avatar} width='100' height='100' />
+//           ) : (
+//             <p>{`No Avatar Available`}</p>
+//           )}
+//         </div>
+//         <div>
+//           {profileUrl ? (
+//             <Link href={profileUrl} target='_blank'>
+//               <p>{`User: ${displayName}`}</p>
+//             </Link>
+//           ) : (
+//             <p>{`User: ${displayName}`}</p>
+//           )}
+//         </div>
+//       </div>
+//     );
+//   }
+//   return <div>No gif available</div>;
+// };
 
-export default GiphyDetail;
+// export default GiphyDetail;
--- src/app/giphy/page.jsx
+++ src/app/giphy/page.jsx
@@ -1,11 +1,60 @@
-'use client';
-// import SearchBar from '../components/searchbar/page';
-// import GiphyList from '../components/giphylist/page';
-// // import Loading from '../loading';
-// // import Error from '../error';
+// 'use client';
+// // import SearchBar from '../components/searchbar/page';
+// // import GiphyList from '../components/giphylist/page';
+// // // import Loading from '../loading';
+// // // import Error from '../error';
 
-import giphy from '../apis/giphy';
-// import React, { useState, useEffect } from 'react';
+// import giphy from '../apis/giphy';
+// // import React, { useState, useEffect } from 'react';
+// // import {
+// //   QueryClient,
+// //   QueryClientProvider,
+// //   useQuery,
+// // } from '@tanstack/react-query';
+
+// // const queryClient = new QueryClient();
+// // const Giphy = () => {
+// // const all = useQuery(
+// //   ['giphys'], // Unique query key based on searchTerm
+// //   async () => {
+// //     // if (!searchTerm) return []; // Don't fetch if searchTerm is empty
+
+// //     const response = await giphy.get('/v1/gifs/search', {
+// //       params: {
+// //         q: searchTerm,
+// //       },
+// //     });
+// //     return response.data.data;
+// //   }
+// // );
+
+// //   console.log('hi', all);
+
+// //   // const onSearchSubmit = (term) => {
+// //   //   // Update the searchTerm and invalidate the query
+// //   //   setSearchTerm(term);
+// //   //   queryClient.invalidateQueries(['giphys', term]);
+// //   // };
+
+// //   // if (isLoading) {
+// //   //   return <div>Loading...</div>;
+// //   // }
+
+// //   // if (isError) {
+// //   //   return <div>Error: {error.message}</div>;
+// //   // }
+
+// //   return (
+// //     <div>
+// //       <div>
+// //         {/* <SearchBar onSubmit={onSearchSubmit} />
+// //         <GiphyList giphys={giphys} /> */}
+// //       </div>
+// //     </div>
+// //   );
+// // };
+// // export default Giphy;
+
 // import {
 //   QueryClient,
 //   QueryClientProvider,
@@ -13,86 +62,37 @@ import giphy from '../apis/giphy';
 // } from '@tanstack/react-query';
 
 // const queryClient = new QueryClient();
-// const Giphy = () => {
-// const all = useQuery(
-//   ['giphys'], // Unique query key based on searchTerm
-//   async () => {
-//     // if (!searchTerm) return []; // Don't fetch if searchTerm is empty
 
-//     const response = await giphy.get('/v1/gifs/search', {
-//       params: {
-//         q: searchTerm,
-//       },
-//     });
-//     return response.data.data;
-//   }
-// );
+// export default function Giphy() {
+//   return (
+//     <QueryClientProvider client={queryClient}>
+//       <Example />
+//     </QueryClientProvider>
+//   );
+// }
 
-//   console.log('hi', all);
+// function Example() {
+//   const { isLoading, error, data } = useQuery({
+//     queryKey: ['giphys'],
+//     queryFn: async () => {
+//       const response = await giphy.get('/v1/gifs/search', {
+//         params: {
+//           q: 'hello',
+//         },
+//       });
+//       return response.data.data;
+//     },
+//   });
 
-//   // const onSearchSubmit = (term) => {
-//   //   // Update the searchTerm and invalidate the query
-//   //   setSearchTerm(term);
-//   //   queryClient.invalidateQueries(['giphys', term]);
-//   // };
+//   if (isLoading) return 'Loading...';
 
-//   // if (isLoading) {
-//   //   return <div>Loading...</div>;
-//   // }
-
-//   // if (isError) {
-//   //   return <div>Error: {error.message}</div>;
-//   // }
+//   if (error) return 'An error has occurred: ' + error.message;
 
 //   return (
 //     <div>
-//       <div>
-//         {/* <SearchBar onSubmit={onSearchSubmit} />
-//         <GiphyList giphys={giphys} /> */}
-//       </div>
+//       <h1>{data.name}</h1>
+//       <p>{data.description}</p>
+   
 //     </div>
 //   );
-// };
-// export default Giphy;
-
-import {
-  QueryClient,
-  QueryClientProvider,
-  useQuery,
-} from '@tanstack/react-query';
-
-const queryClient = new QueryClient();
-
-export default function Giphy() {
-  return (
-    <QueryClientProvider client={queryClient}>
-      <Example />
-    </QueryClientProvider>
-  );
-}
-
-function Example() {
-  const { isLoading, error, data } = useQuery({
-    queryKey: ['giphys'],
-    queryFn: async () => {
-      const response = await giphy.get('/v1/gifs/search', {
-        params: {
-          q: 'hello',
-        },
-      });
-      return response.data.data;
-    },
-  });
-
-  if (isLoading) return 'Loading...';
-
-  if (error) return 'An error has occurred: ' + error.message;
-
-  return (
-    <div>
-      <h1>{data.name}</h1>
-      <p>{data.description}</p>
-   
-    </div>
-  );
-}
+// }
