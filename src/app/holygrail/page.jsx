d30b514aa15a02443ca477be8fcc9eb59ecc2a6c
Dave Lin <dave.lin21@gmail.com>
2023-10-01 17:09:04 -0400

    holy grail

---
+++ src/app/holygrail/page.jsx
@@ -0,0 +1,17 @@
+import './styles.css';
+
+const HolyGrail = () => {
+  return (
+    <>
+      <header>Header</header>
+      <x className='containerx'>
+        <nav>Navigation</nav>
+        <main>Main</main>
+        <aside>Sidebar</aside>
+      </x>
+      <footer>Footer</footer>
+    </>
+  );
+};
+
+export default HolyGrail;
